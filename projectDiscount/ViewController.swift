5a17e1e64b2e4249b444cd621b2aba4129bfcdf0
MacDetail <supakorndev15@gmail.com>
2025-07-09 10:11:07 +0700

    no message

---
+++ projectDiscount/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  projectDiscount
+//
+//  Created by MacDetail on 3/7/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var txtLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculateDiscount()
+    }
+
+    func calculateDiscount() {
+        let cart = [
+            Item(name: "T-Shirt", category: .clothing, price: 350),
+            Item(name: "Hat", category: .accessories, price: 250),
+            Item(name: "Belt", category: .accessories, price: 230)
+        ]
+
+        let campaigns: [Campaign] = [
+            Campaign(type: .percentage(percent: 10), group: .coupon),
+            Campaign(type: .pointDiscount(points: 68), group: .onTop),
+            Campaign(type: .seasonalDiscount(everyX: 300, discountY: 40), group: .seasonal)
+        ]
+
+        let calculator = DiscountCalculator()
+        let finalPrice = calculator.applyDiscounts(cart: cart, campaigns: campaigns)
+        print("Final price: \(finalPrice) THB")
+        
+        txtLabel.text = "Final price: \(finalPrice) THB"
+    }
+}
+
+
